e83f09c0fdd0135fdfe78e995a6d7ca5747f47cd
Muhammadsheraz492 <90733116+Muhammadsheraz492@users.noreply.github.com>
2021-12-11 21:59:12 +0500

    Threen Screen

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import Constants from 'expo-constants';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // add firebase config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCdZnV0xyVyL-dOliRCMC5WOwCaeJrjJbU",
+//   authDomain: "completefirebase-29443.firebaseapp.com",
+//   projectId: "completefirebase-29443",
+//   storageBucket: "completefirebase-29443.appspot.com",
+//   messagingSenderId: "1057136108382",
+//   appId: "1:1057136108382:web:9f5caed5f3642722bda5cf",
+// };
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId
+  apiKey: "AIzaSyCVtMmMrE-T0Sal2HemnORZ2hqpQ8KrRuc",
+  authDomain: "services-18245.firebaseapp.com",
+  projectId: "services-18245",
+  storageBucket: "services-18245.appspot.com",
+  messagingSenderId: "286212564782",
+  appId: "1:286212564782:web:4903a087accc602d617cbf",
 };
-
 // initialize firebase
 initializeApp(firebaseConfig);
 
